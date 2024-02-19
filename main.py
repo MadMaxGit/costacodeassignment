e1a1850c3868bb3d3385488991907a453d321bc1
MadMaxGit <kpmanjunath1024@gmail.com>
2024-02-19 15:27:47 +0100

    Created a Skeleton for the Query Schema

--- main.py
+++ main.py
@@ -32,33 +32,24 @@ class VehicleEntity(ObjectType):
     chassisNumber = String()
     cassisSeries = String()
     services = List(ServiceInfo)
-    
+
 class UserType(ObjectType):
     id = Int()
     name= String()
     age=Int()
     place= String()
 
+
 class Query(ObjectType):
-    user = Field(UserType, user_id=Int())
-    #user_by_min_age = List(UserType, min_age=Int())
+ 
+    def resolve_vehicleById(self, info, vehicleId):
+        pass
 
-    #dummy data store
-    users = [
-        {"id":1, "name": "Man", "age": 33},
-        {"id":2, "name": "Shi", "age": 31},
-        {"id":3, "name": "Tri", "age": 3, "place": "Bang"},
-        {"id":4, "name": "Ok", "age": 29}
-    ]
+    def resolve_vehiclesByPartialName(self, info, partialName):
+        pass
 
-    @staticmethod
-    def resolve_user(root, info, user_id):
-        matched_users = [user for user in Query.users if user["id"] == user_id]
-        return matched_users[0] if matched_users else None
-    
-    @staticmethod
-    def resolve_user_by_min_age(root, info, min_age):
-        return [user for user in Query.users if user["age"] >= min_age]
+    def resolve_vehiclesByServiceStatus(self, info, serviceName, serviceStatus):
+        pass
 
 gpl_query='''
 query{
