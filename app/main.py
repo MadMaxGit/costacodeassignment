d801893a6e542b15fe378979eefdcd3856128e09
MadMaxGit <kpmanjunath1024@gmail.com>
2024-02-20 09:43:24 +0100

    Added Loguru for the logs.

--- app/main.py
+++ app/main.py
@@ -3,7 +3,12 @@ from app.graphql.queries import Query
 from fastapi import FastAPI
 from starlette_graphene3 import GraphQLApp, make_playground_handler
 
-mockoon_url = 'http://localhost:1337/vehicle/info'
+from loguru import logger
+from app.vehiclemock import VehicleMock
+from app.db.database import collection
+
+#Adding a Logger Library for logging
+logger.add("app.log", rotation="500 MB")
 
 schema = Schema(query=Query) #Graphene is a Python library for building GraphQL APIs
 
