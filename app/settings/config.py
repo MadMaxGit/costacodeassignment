206074429f69fb7e3bf775ec15f7f15fa0f0b129
Manjunath Kakkepalya Puttaswamy <85114693+MadMaxGit@users.noreply.github.com>
2024-03-01 09:45:36 +0100

    Update config.py

--- app/settings/config.py
+++ app/settings/config.py
@@ -1,3 +1,3 @@
 #Work on the encoding the password of the DB
-DB_URL="mongodb+srv://root:root@cluster0.8vufshx.mongodb.net/?retryWrites=true&w=majority"
+DB_URL="mongodb+srv://root:<password>@cluster0.8vufshx.mongodb.net/?retryWrites=true&w=majority" #fill the password
 mockoon_URL="http://localhost:1337/vehicle/"
